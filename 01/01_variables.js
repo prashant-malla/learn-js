4975f54438980541ccf0407bcda8cf89504c9c8d
Prashant Malla <prashant.malla977@gmail.com>
2024-09-03 16:34:25 +0000

    primitive data type

---
+++ 01/01_variables.js
@@ -0,0 +1,45 @@
+let fullName = "ram";
+const office = 'test'
+
+function greet() {
+    const office = 'test1'
+    console.log(office)
+    var rollNo = 211
+    // fullName = 'test'
+
+    // console.log(fullName)
+    // console.log(rollNo)
+}
+
+const office = 'test2'
+greet()
+console.log(office)
+
+// let is block scope {}
+// if (true) {
+//     let fullName = 'john'
+//     var age = 21
+//     console.log(fullName) // john
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+// var fullName = 'shyam' // re-declared
+
+// name = 'john' // updated/ re-assigned
+
+// greet()
+// console.log(fullName) // ram
+// console.log(age)
+// console.log(rollNo)
+
+// scope
+// var : global or function
+// let, const: block scope
+
+// updated and re-declared
+// var can be updated and re-declared within its scope
+// let variable can be updated but not re-declared
+// const can neither be updated nor re-declared
---
+++ 01/02_datatypes.js
@@ -0,0 +1,7 @@
+let name = 'john' //string
+let age = '22' // number
+let isLoggedIn = false // boolean
+let college; // undefine
+let phone = null; // null(is a standalone value)
+
+console.log(typeof (phone)) 
