f5235b980f6be42bbdb4e61bc94bca9bf5f6cd63
Prashant Malla <prashant.malla977@gmail.com>
2024-09-03 17:03:49 +0000

    comment for datatype

--- 01/02_datatypes.js
+++ 01/02_datatypes.js
@@ -1,7 +1,20 @@
+// Seven data types that are primitives:
+// Boolean: true and false.
+// null: A special keyword denoting a null value. (Because JavaScript is case -sensitive, null is not the same as Null, NULL, or any other variant.)
+// undefined: A top - level property whose value is not defined.
+// Number.: An integer or floating point number.For example: 42 or 3.14159.
+// BigInt: An integer with arbitrary precision.For example: 9007199254740992n.
+// String: A sequence of characters that represent a text value.For example: "Howdy".
+// Symbol: A data type whose instances are unique and immutable.
+
+// and Object
+
+
 let name = 'john' //string
-let age = '22' // number
+let age = 22 // number
 let isLoggedIn = false // boolean
 let college; // undefine
 let phone = null; // null(is a standalone value)
 
-console.log(typeof (phone)) 
+console.log(1 + 3 + "6")
+console.log(typeof (age)) 
